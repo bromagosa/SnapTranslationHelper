0b935533037bc071a5f2fc9f3d66773234966283
Bernat Romagosa <bromagosa@e-citilab.eu>
2020-06-11 07:33:06 +0200

    Preseve original comment and lang key, find translations in destinationTree AST, instead of in SnapTranslator, to fix issues with concatenated strings

--- translationHelper.js
+++ translationHelper.js
@@ -14,30 +14,40 @@ function readFile (fileName) {
     return fs.readFileSync(__dirname + '/' + fileName, { encoding: 'utf-8' });
 }
 
-// Parse and load source locale js file
-const source = readFile(sourceFiles[0]);
-eval(source);
-const tree = recast.parse(source);
+// Parse source and destination locale js files
+const sourceTree = recast.parse(readFile(sourceFiles[0])).program.body[0];
+const destinationTree = recast.parse(readFile(sourceFiles[1])).program.body[0];
 
-// Just load destination locale js file
-eval(readFile(sourceFiles[1]));
+// Keep original comments
+sourceTree.comments = destinationTree.comments;
+
+// Keep left side of the assignment, thus preserving the language key
+sourceTree.expression.left = destinationTree.expression.left;
 
 // Search for missing strings by traversing the destination tree to reach the
-// keys in the dictionary. This works because we know how lang files are
-// structured.
+// keys in the dictionary.
 // The body of the program has a single assignment expression, where the right
 // side of the expression is the dictionary. We can then traverse the properties
 // of that dictionary and ask for the content (value, in AST terms) of each key.
-tree.program.body[0].expression.right.properties.forEach(
+sourceTree.expression.right.properties.forEach(
     (each) => {
         let sourceString = each.key.value,
-            translation = SnapTranslator.dict[destinationLang][sourceString];
-        each.value.value = translation || ('MISSING: ' + each.key.value);
+            destinationProperty =
+                destinationTree.expression.right.properties.find(
+                    (association) => {
+                        return association.key.value == each.key.value
+                    }
+                );
+        if (destinationProperty) {
+            each.value = destinationProperty.value;
+        } else {
+            each.value.value = '';
+        }
     }
 );
 
 // Now export the modified AST into a new file
 fs.writeFileSync(
     'lang-' + destinationLang + '-editme.js',
-    recast.print(tree).code
+    recast.print(sourceTree, { quote: 'single' }).code
 );
